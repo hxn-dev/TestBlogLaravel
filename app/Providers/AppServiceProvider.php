6335c899ac4602c16904acad19b2b582a5c665f2
Jonathan V <hxn@live.fr>
2017-10-29 02:02:54 +0100

    change view composer, only show categories who has articles

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         view()->composer('layouts.sidebar', function($view) {
-            $view->with('categories', \App\Category::all());
+            $view->with('categories', \App\Category::has('articles')->get());
         });
     }
 
